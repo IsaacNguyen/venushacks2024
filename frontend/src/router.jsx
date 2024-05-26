1203f004cbd26ab3c3f45f31a922198f3ae64700
Robert Gilmore <robbiefive@hotmail.com>
2024-05-25 22:55:16 -0700

    added js features

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,8 @@
 
 import Main from './pages/Main.jsx'
 import AddressInput from './pages/AddressInput.jsx'
+import Tests from './pages/Tests.jsx'
+import Graph from './pages/Graph.jsx'
 import {
     createBrowserRouter
   } from "react-router-dom";
@@ -14,5 +16,13 @@ import {
     {
       path:'/address',
       element: <AddressInput/>,
-    }
+    },
+    {
+      path:'/tests',
+      element: <Tests/>,
+    },
+    {
+      path:'/graph',
+      element: <Graph/>,
+    },
   ]);
