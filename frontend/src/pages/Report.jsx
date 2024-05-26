6d495a05374f1bdc2b35149ef04cbe771e83a16c
IsaacNguyen <isaachugh33@gmail.com>
2024-05-26 05:05:21 -0700

    robbie line graphhhh

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,4 +1,4 @@
-
+import { Line } from 'react-chartjs-2';
 import MooScore from './MooScore';
 import ReportInfo from './ReportInfo';
 import styles from './styles/Report.module.css';
@@ -9,6 +9,28 @@ const beds = propData.Records[0].IntRoomInfo.BedroomsCount;
 const sqft = propData.Records[0].PropertySize.AreaBuilding;
 const stories = propData.Records[0].IntRoomInfo.StoriesCount;
 const owner = propData.Records[0].PrimaryOwner.Name1Full;
+
+const defaultYear = "N/A";
+const defaultValue = 0;
+
+function getLabelOrDefault(record, path, defaultValue) {
+    return path.reduce((acc, key) => (acc && acc[key] !== undefined) ? acc[key] : defaultValue, record);
+}
+
+function getDataOrDefault(record, path, defaultValue) {
+    const value = path.reduce((acc, key) => (acc && acc[key] !== undefined) ? acc[key] : defaultValue, record);
+    return typeof value === 'number' ? value : +value;
+}
+
+const valuationYear = getLabelOrDefault(propData.Records[0], ['EstimatedValue', 'ValuationDate'], defaultYear).slice(0, 4);
+const yearAssessed = getLabelOrDefault(propData.Records[0], ['Tax', 'YearAssessed'], defaultYear);
+const lastSaleDate = getLabelOrDefault(propData.Records[0], ['SaleInfo', 'AssessorLastSaleDate'], defaultYear).slice(0, 4);
+const futureYear = String(+valuationYear + 5);
+
+const estimatedValue = getDataOrDefault(propData.Records[0], ['EstimatedValue', 'EstimatedValue'], defaultValue);
+const assessedValueTotal = getDataOrDefault(propData.Records[0], ['Tax', 'AssessedValueTotal'], defaultValue);
+const lastSaleAmount = getDataOrDefault(propData.Records[0], ['SaleInfo', 'AssessorLastSaleAmount'], defaultValue);
+const futureValue = (estimatedValue - assessedValueTotal) * (1 + 0.04) ** 5 + estimatedValue;
 
 function Report(data){
     return(
@@ -49,7 +71,22 @@ function Report(data){
             </div>
             <div className = {styles.bottom}>
             <div className= {styles.mortgagec}>
-             
+                <div>
+                    <Line
+                        data={{
+                            labels: [lastSaleDate, yearAssessed, valuationYear, futureYear],
+                            datasets: [
+                                {
+                                    label: "Property Value Projection",
+                                    data: [lastSaleAmount, assessedValueTotal,  estimatedValue, futureValue],
+                                    fill: false,
+                                    borderColor: 'rgb(75, 192, 192)',
+                                    tension: 0.1
+                                },
+                            ],
+                        }}
+                    />
+                    </div>
               <small>what's a mortgage?</small>
               </div>
             </div>
